4845ca59270eda97f756701c7099ef038e6d8783
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-04-29 16:27:55 +0800

    simplify the true cases into a single line

--- DC.Lab/CanalLock.cs
+++ DC.Lab/CanalLock.cs
@@ -19,10 +19,8 @@ public class CanalLock
         HighWaterGateOpen = (open, HighWaterGateOpen, CanalLockWaterLevel) switch
         {
             (false, _, _) => false,
-            (true, false, WaterLevel.High) => true,
+            (true, _, WaterLevel.High) => true,
             (true, false, WaterLevel.Low) => throw new InvalidOperationException("Cannot open high gate when the water is low"),
-            (true, true, WaterLevel.High) => true,
-            (true, true, WaterLevel.Low) => false, // should never happen
             _ => throw new InvalidOperationException("Invalid internal state"),
         };
     }
