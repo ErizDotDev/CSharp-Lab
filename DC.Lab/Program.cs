f9c48a6736404aa5cddbbdfe4f33f68434bd673d
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-04-29 16:04:58 +0800

    create tests for CanalLock

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using DC.Lab;
+
+// Create a new canal lock:
+var canalGate = new CanalLock();
+
+// State should be doors closed, water level low:
+Console.WriteLine(canalGate);
+
+canalGate.SetLowGate(open: true);
+Console.WriteLine($"Open the lower gate: {canalGate}");
+
+Console.WriteLine("Boat enters lock from lower gate");
+
+canalGate.SetLowGate(open: false);
+Console.WriteLine($"Close the lower gate: {canalGate}");
+
+canalGate.SetWaterLevel(WaterLevel.High);
+Console.WriteLine($"Raise the water level: {canalGate}");
+
+canalGate.SetHighGate(open: true);
+Console.WriteLine($"Open the higher gate: {canalGate}");
+
+Console.WriteLine("Boat exits lock at upper gate");
+Console.WriteLine("Boat enters lock from upper gate");
+
+canalGate.SetHighGate(open: false);
+Console.WriteLine($"Close the higher gate: {canalGate}");
+
+canalGate.SetWaterLevel(WaterLevel.Low);
+Console.WriteLine($"Lower the water level: {canalGate}");
+
+canalGate.SetLowGate(open: true);
+Console.WriteLine($"Open the lower gate: {canalGate}");
+
+Console.WriteLine("Boat exits lock at upper gate.");
+
+canalGate.SetLowGate(open: false);
+Console.WriteLine($"Close the lower gate: {canalGate}");
