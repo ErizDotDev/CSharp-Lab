fa04f06da212dedf5fbcd0086e6736307198ceb0
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-04-29 16:04:41 +0800

    create class for CanalLock

---
+++ DC.Lab/CanalLock.cs
@@ -0,0 +1,38 @@
+﻿namespace DC.Lab;
+
+public enum WaterLevel
+{
+    Low,
+    High
+}
+
+public class CanalLock
+{
+    // Query canal lock state:
+    public WaterLevel CanalLockWaterLevel { get; private set; } = WaterLevel.Low;
+    public bool HighWaterGateOpen { get; private set; } = false;
+    public bool LowWaterGateOpen { get; private set; } = false;
+
+    // Change the upper gate.
+    public void SetHighGate(bool open)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Change the low gate.
+    public void SetLowGate(bool open)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Change water level.
+    public void SetWaterLevel(WaterLevel newLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString() =>
+        $"The lower gate is {(LowWaterGateOpen ? "Open" : "Closed")}. " +
+        $"The upper gate is {(HighWaterGateOpen ? "Open" : "Closed")}. " +
+        $"The water level is {CanalLockWaterLevel}";
+}
