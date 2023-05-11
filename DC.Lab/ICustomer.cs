fcc09c1c899765af61ba39b2068bc26d24eacbcc
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-05-12 02:35:07 +0800

    add forgotten code in main

--- DC.Lab/ICustomer.cs
+++ DC.Lab/ICustomer.cs
@@ -8,4 +8,13 @@ public interface ICustomer
     DateTime? LastOrder { get; }
     string Name { get; }
     IDictionary<DateTime, string> Reminders { get; }
+
+    public decimal ComputeLoyaltyDiscount()
+    {
+        DateTime TwoYearsAgo = DateTime.Now.AddYears(-2);
+        if ((DateJoined < TwoYearsAgo) && (PreviousOrders.Count() > 10))
+            return 0.10m;
+
+        return 0;
+    }
 }
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using DC.Lab;
+
+var c = new SampleCustomer("customer one", new DateTime(2010, 5, 31))
+{
+    Reminders =
+    {
+        { new DateTime(2010, 8, 12), "child's birthday" },
+        { new DateTime(2010, 11, 15), "anniversarry" }
+    }
+};
+
+var o = new SampleOrder(new DateTime(2012, 6, 1), 5m);
+c.AddOrder(o);
+
+o = new SampleOrder(new DateTime(2013, 7, 4), 25m);
+c.AddOrder(o);
+
+Console.WriteLine($"Data about {c.Name}");
+Console.WriteLine($"Joined on {c.DateJoined}. Made {c.PreviousOrders.Count()} orders, the last on {c.LastOrder}");
+Console.WriteLine("Reminders");
+
+foreach (var item in c.Reminders)
+{
+    Console.WriteLine($"\t{item.Value} on {item.Key}");
+}
+
+foreach (var order in c.PreviousOrders)
+{
+    Console.WriteLine($"Order on {order.Purchased} for {order.Cost}");
+}
