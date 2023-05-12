9513fdb021207a876066aed3061f7d651a5adda6
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-05-13 03:11:20 +0800

    implementation

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using DC.Lab;
+
+var list = new GenericList<int>();
+
+for (int x = 0; x < 10; x++)
+{
+    list.AddHead(x);
+}
+
+foreach (int i in list)
+{
+    Console.Write($"{i} ");
+}
+
+Console.WriteLine("Done");
