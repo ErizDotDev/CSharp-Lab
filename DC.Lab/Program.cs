2f10c8fe36c9de79b9a6b8be0bef169f832197e7
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-05-21 22:10:00 +0800

    implementation

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿// Polymorphism at work #1: a Rectangle, Triangle and Circle
+// can all be used wherever a Shape is expected. No cast is
+// required because an implicit conversion exists from a derived
+// class to its base class.
+var shapes = new List<Shape>
+{
+    new Rectangle(),
+    new Triangle(),
+    new Circle()
+};
+
+// Polymorphism at work #2: the virtual method Draw is
+// invoked on each of the derived classes, not the base class.
+foreach (var shape in shapes)
+{
+    shape.Draw();
+}
+
+public class Shape
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+
+    public virtual void Draw()
+    {
+        Console.WriteLine("Performing base class drawing tasks");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+        base.Draw();
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+        base.Draw();
+    }
+}
+
+public class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a triangle");
+        base.Draw();
+    }
+}
