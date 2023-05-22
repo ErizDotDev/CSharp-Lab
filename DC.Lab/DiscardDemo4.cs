98890366a9e971e683fe06e4a53ff8ae15dc3e09
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-05-22 21:04:11 +0800

    implement fourth demo

---
+++ DC.Lab/DiscardDemo4.cs
@@ -0,0 +1,25 @@
+﻿namespace DC.Lab;
+
+internal class DiscardDemo4
+{
+    public static async Task Execute()
+    {
+        Console.WriteLine("About to launch a task.");
+
+        _ = Task.Run(() =>
+        {
+            var iterations = 0;
+
+            for (int ctr = 0; ctr < int.MaxValue; ctr++)
+                iterations++;
+
+            Console.WriteLine("Completed looping operation...");
+
+            throw new InvalidOperationException();
+        });
+
+        await Task.Delay(5000);
+
+        Console.WriteLine("Exiting after 5 seconds display.");
+    }
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,7 +1,10 @@
-﻿using DC.Lab;
-
+﻿
 // DiscardDemo1.Execute();
 
 // DiscardDemo2.Execute();
 
-DiscardDemo3.Execute();
+// DiscardDemo3.Execute();
+
+using DC.Lab;
+
+await DiscardDemo4.Execute();
