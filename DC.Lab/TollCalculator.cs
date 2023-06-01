73b7153745c1108c2994634e7b6db01375200074
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-02 02:13:52 +0800

    implement short version of pattern matching switch

--- DC.Lab/TollCalculator.cs
+++ DC.Lab/TollCalculator.cs
@@ -9,15 +9,21 @@ public class TollCalculator
     public decimal CalculateToll(object vehicle) =>
         vehicle switch
         {
-            Car { Passengers: 0 } => 2.00m + 0.50m,
-            Car { Passengers: 1 } => 2.00m,
-            Car { Passengers: 2 } => 2.00m - 0.50m,
-            Car => 2.00m - 1.0m,
+            Car c => c.Passengers switch
+            {
+                0 => 2.00m + 0.5m,
+                1 => 2.00m,
+                2 => 2.00m - 0.5m,
+                _ => 2.00m - 1.0m
+            },
 
-            Taxi { Fares: 0 } => 3.50m + 0.50m,
-            Taxi { Fares: 1 } => 3.50m,
-            Taxi { Fares: 2 } => 3.50m - 0.50m,
-            Taxi => 3.50m - 1.0m,
+            Taxi t => t.Fares switch
+            {
+                0 => 3.50m + 0.5m,
+                1 => 3.50m,
+                2 => 3.50m - 0.5m,
+                _ => 3.50m - 1.0m
+            },
 
             Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
             Bus b when ((double)b.Riders / (double)b.Capacity) < 0.90 => 5.00m - 1.00m,
