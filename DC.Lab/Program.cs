8f0527822f0d649fcea2f805687259f74e304752
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-02 02:10:07 +0800

    implement long version of pattern matching switch

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using Calculators;
+using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+
+var tollCalc = new TollCalculator();
+
+var car = new Car();
+var taxi = new Taxi();
+var bus = new Bus();
+var truck = new DeliveryTruck();
+
+Console.WriteLine($"The toll for a car is {tollCalc.CalculateToll(car)}");
+Console.WriteLine($"The toll for a taxi is {tollCalc.CalculateToll(taxi)}");
+Console.WriteLine($"The toll for a bus is {tollCalc.CalculateToll(bus)}");
+Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(truck)}");
+
+try
+{
+    tollCalc.CalculateToll("This will fail");
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Caught an argument exception when using the wrong type");
+}
+
+try
+{
+    tollCalc.CalculateToll(null!);
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine("Caught an argument exception when using null");
+}
---
+++ DC.Lab/TollCalculator.cs
@@ -0,0 +1,33 @@
+﻿using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+
+namespace Calculators;
+
+public class TollCalculator
+{
+    public decimal CalculateToll(object vehicle) =>
+        vehicle switch
+        {
+            Car { Passengers: 0 } => 2.00m + 0.50m,
+            Car { Passengers: 1 } => 2.00m,
+            Car { Passengers: 2 } => 2.00m - 0.50m,
+            Car => 2.00m - 1.0m,
+
+            Taxi { Fares: 0 } => 3.50m + 0.50m,
+            Taxi { Fares: 1 } => 3.50m,
+            Taxi { Fares: 2 } => 3.50m - 0.50m,
+            Taxi => 3.50m - 1.0m,
+
+            Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
+            Bus b when ((double)b.Riders / (double)b.Capacity) < 0.90 => 5.00m - 1.00m,
+            Bus b => 5.00m,
+
+            DeliveryTruck t when (t.GrossWeightClass > 5000) => 10.00m + 5.00m,
+            DeliveryTruck t when (t.GrossWeightClass < 3000) => 10.00m - 2.00m,
+            DeliveryTruck t => 10.00m,
+
+            { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
+            null => throw new ArgumentNullException(nameof(vehicle)),
+        };
+}
