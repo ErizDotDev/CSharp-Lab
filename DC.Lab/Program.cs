89755de5665722a1d809067a40b8bc4d98003060
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-08 03:19:17 +0800

    implementation

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,79 @@
-﻿
+﻿using DC.Lab;
+
+await RunTeleprompter();
+
+static async Task RunTeleprompter()
+{
+    var config = new TelePrompterConfig();
+    var displayTask = ShowTeletrompter(config);
+
+    var speedTask = GetInput(config);
+    await Task.WhenAny(displayTask, speedTask);
+}
+
+static async Task ShowTeletrompter(TelePrompterConfig config)
+{
+    var words = ReadFrom("SampleQuotes.txt");
+
+    foreach (var word in words)
+    {
+        Console.WriteLine(word);
+
+        if (!string.IsNullOrWhiteSpace(word))
+            await Task.Delay(config.DelayInMilliseconds);
+    }
+
+    config.SetDone();
+}
+
+static IEnumerable<string> ReadFrom(string file)
+{
+    string? line;
+
+    using (var reader = File.OpenText(file))
+    {
+        while ((line = reader.ReadLine()) != null)
+        {
+            var words = line.Split(' ');
+            var lineLength = 0;
+
+            foreach (var word in words)
+            {
+                yield return word + " ";
+
+                lineLength += word.Length + 1;
+
+                if (lineLength > 70)
+                {
+                    yield return Environment.NewLine;
+                    lineLength = 0;
+                }
+            }
+
+            yield return Environment.NewLine;
+        }
+    }
+}
+
+static async Task GetInput(TelePrompterConfig config)
+{
+    var delay = 200;
+
+    Action work = () =>
+    {
+        do
+        {
+            var key = Console.ReadKey(true);
+
+            if (key.KeyChar == '>')
+                config.UpdateDelay(10);
+            else if (key.KeyChar == '<')
+                config.UpdateDelay(10);
+            else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                config.SetDone();
+        }
+        while (!config.Done);
+    };
+
+    await Task.Run(work);
+}
---
+++ DC.Lab/TelePrompterConfig.cs
@@ -0,0 +1,22 @@
+﻿using static System.Math;
+
+namespace DC.Lab;
+
+internal class TelePrompterConfig
+{
+    public int DelayInMilliseconds { get; private set; } = 200;
+
+    public bool Done { get; private set; }
+
+    public void UpdateDelay(int increment)
+    {
+        var newDelay = Min(DelayInMilliseconds + increment, 100);
+        newDelay = Max(newDelay, 20);
+        DelayInMilliseconds = newDelay;
+    }
+
+    public void SetDone()
+    {
+        Done = true;
+    }
+}
