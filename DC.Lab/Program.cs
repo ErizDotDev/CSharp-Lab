f3ca9eb68ea9ecba7b917d5de16fa72e49d85490
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-11 23:25:18 +0800

    delay await async methods

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -13,19 +13,23 @@ class Program
         Coffee cup = PourCoffee();
         Console.WriteLine("coffee is ready");
 
-        Egg eggs = await FryEggsAsync(2);
-        Console.WriteLine("eggs are ready");
+        Task<Toast> toastTask = ToastBreadAsync(2);
+        Task<Egg> eggTask = FryEggsAsync(2);
+        Task<Bacon> baconTask = FryBaconAsync(3);
 
-        Bacon bacon = await FryBaconAsync(3);
-        Console.WriteLine("bacon is ready");
-
-        Toast toast = await ToastBreadAsync(2);
+        Toast toast = await toastTask;
         ApplyButter(toast);
         ApplyJam(toast);
         Console.WriteLine("toast is ready");
 
         Juice oj = PourOJ();
         Console.WriteLine("oj is ready");
+
+        Egg eggs = await eggTask;
+        Console.WriteLine("eggs are ready");
+
+        Bacon bacon = await baconTask;
+        Console.WriteLine("bacon is ready");
 
         Console.WriteLine("Breakfast is ready!");
 
