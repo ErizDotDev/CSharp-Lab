448266273c129451915bd8ac4fc4c9a3e78274fa
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-23 19:30:18 +0800

    implementation

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace DC.Lab
+{
+    class Program
+    {
+        static int State = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Value of State before assignment: {State}");
+
+            SetState();
+
+            Console.WriteLine($"Value of State after reassignment: {State}");
+        }
+
+        static void SetState()
+        {
+            State = 1;
+        }
+    }
+}
