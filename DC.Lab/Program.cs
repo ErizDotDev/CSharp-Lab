3ee5d501379b837747dd702263ba6a66983ec7ad
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-27 08:43:34 +0800

    implementation of levels

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -27,6 +27,29 @@
 
             ctx = ctxProvider.GetContext();
             Console.WriteLine($"Current context for id: {id} - {ctx.Id}");
+
+            await Level1(id);
+        }
+
+        static async Task Level1(int id)
+        {
+            await Task.Delay(r.Next(1000));
+
+            var ctx = ctxProvider.GetContext();
+            ctx.Name = $"MyName{id}";
+
+            Console.WriteLine($"Level 1 - Current context for id: {id} - {ctx.Id}");
+
+            await Level2(id);
+        }
+
+        static async Task Level2(int id)
+        {
+            await Task.Delay(r.Next(1000));
+
+            var ctx = ctxProvider.GetContext();
+
+            Console.WriteLine($"Level 2 - Current context for id: {id} - {ctx.Id}, name {ctx.Name}");
         }
     }
 
@@ -46,5 +69,6 @@
     public class Context
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
