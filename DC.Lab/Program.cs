9e864500908bf6343727a05544d52404518be253
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-06-27 08:35:00 +0800

    simple implementation of async local

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿namespace DC.Lab
+{
+    class Program
+    {
+        static Random r = new Random();
+        static ContextProvider<Context> ctxProvider = new ContextProvider<Context>();
+
+        static void Main(string[] args)
+        {
+            InitializeAndPrintContextAsync(1);
+            InitializeAndPrintContextAsync(2);
+            InitializeAndPrintContextAsync(3);
+
+            Console.Read();
+        }
+
+        static async Task InitializeAndPrintContextAsync(int id)
+        {
+            var ctx = ctxProvider.InitContext();
+            var guid = Guid.NewGuid();
+
+            Console.WriteLine($"Init context for id: {id} - {guid}");
+
+            ctx.Id = guid;
+
+            await Task.Delay(r.Next(1000));
+
+            ctx = ctxProvider.GetContext();
+            Console.WriteLine($"Current context for id: {id} - {ctx.Id}");
+        }
+    }
+
+    class ContextProvider<T> where T : new()
+    {
+        public T InitContext()
+        {
+            _asyncLocal.Value = new T();
+            return _asyncLocal.Value;
+        }
+
+        public T GetContext() => _asyncLocal.Value!;
+
+        private static readonly AsyncLocal<T> _asyncLocal = new AsyncLocal<T>();
+    }
+
+    public class Context
+    {
+        public Guid Id { get; set; }
+    }
+}
