60a4a4e467d6ccc1bd1e8686ed3f820a91a66c5b
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-07-13 15:20:01 +0800

    implement method syntax for group sub querying

--- DC.Lab/GroupSubQuery.cs
+++ DC.Lab/GroupSubQuery.cs
@@ -5,6 +5,8 @@ public class GroupSubQuery
     public static void Execute()
     {
         var students = Student.students;
+
+        // Query syntax
         var queryGroupMax =
             from student in students
             group student by student.Year into studentGroup
@@ -16,6 +18,17 @@ public class GroupSubQuery
                     select student2.ExamScores.Average()
                 ).Max()
             };
+
+        // Method syntax
+        var queryGroupMin =
+            students
+                .GroupBy(s => s.Year)
+                .Select(studentGroup => new
+                {
+                    Level = studentGroup.Key,
+                    HighestScore = studentGroup
+                        .Select(s2 => s2.ExamScores.Average()).Min()
+                });
 
         int count = queryGroupMax.Count();
         Console.WriteLine($"Number of groups = {count}");
