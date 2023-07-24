d63785c4e0dc589e5568ed59dbf570e8537b7012
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-07-24 18:21:32 +0800

    implement demo of Select vs. SelectMany

--- DC.Lab/ProjectionOperation.cs
+++ DC.Lab/ProjectionOperation.cs
@@ -4,7 +4,8 @@ public class ProjectionOperation
 {
     public static void Execute()
     {
-        DemoSimpleZip();
+        //DemoSimpleZip();
+        DemoSelectVsSelectMany();
     }
 
     public static void DemoSimpleZip()
@@ -19,9 +20,39 @@ public class ProjectionOperation
             'A', 'B', 'C', 'D', 'E', 'F'
         };
 
-        foreach ((char letter, int number) in letters.Zip(numbers))
+        foreach ((int number, char letter) in numbers.Zip(letters))
         {
             Console.WriteLine($"Number {number} zipped with letter {letter}");
         }
     }
+
+    public static void DemoSelectVsSelectMany()
+    {
+        List<Bouquet> bouquets = new()
+        {
+            new Bouquet { Flowers = new List<string> { "sunflower", "daisy", "daffodil", "larkspur" } },
+            new Bouquet { Flowers = new List<string> { "tulip", "rose", "orchid" }},
+            new Bouquet { Flowers = new List<string> { "gladiolis", "lily", "snapdragon", "aster", "protea" }},
+            new Bouquet { Flowers = new List<string> { "larkspur", "lilac", "iris", "dahlia" }}
+        };
+
+        IEnumerable<List<string>> query1 = bouquets.Select(b => b.Flowers);
+        IEnumerable<string> query2 = bouquets.SelectMany(b => b.Flowers);
+
+        Console.WriteLine("Results by using Select():");
+
+        foreach (IEnumerable<string> collection in query1)
+            foreach (string item in collection)
+                Console.WriteLine(item);
+
+        Console.WriteLine("\nResults by using SelectMany():");
+
+        foreach (string item in query2)
+            Console.WriteLine(item);
+    }
+}
+
+class Bouquet
+{
+    public List<string> Flowers { get; set; }
 }
