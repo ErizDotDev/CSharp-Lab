52a4153326e1fc5f273ed940083b7514192617c6
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-08-08 19:05:46 +0800

    implement query size

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //FindFileByExtension.Execute();
-        GroupByExtension.Execute();
+        //GroupByExtension.Execute();
+        QuerySize.Execute();
     }
 }
---
+++ DC.Lab/QuerySize.cs
@@ -0,0 +1,49 @@
+﻿namespace DC.Lab;
+
+public class QuerySize
+{
+    internal static void Execute()
+    {
+        string startFolder = @"C:\Program Files\Microsoft Visual Studio\2022\Community\VC#";
+        var fileList = Directory.GetFiles(startFolder, "*.*", SearchOption.AllDirectories);
+
+        var fileQuery = from file in fileList
+                        select GetFileLength(file);
+
+        // Cache the results to avoid multiple trips to the file system.
+        long[] fileLengths = fileQuery.ToArray();
+
+        // Return the size of the largest file
+        long largestFile = fileLengths.Max();
+
+        // Return the total number of bytes in all the files under the specified folder.
+        long totalBytes = fileLengths.Sum();
+
+        Console.WriteLine($"There are {totalBytes} bytes in {fileList.Count()} files under {startFolder}");
+        Console.WriteLine($"The largest files is {largestFile} bytes.");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    // This method is used to swallow the possible exception
+    // that can be raised when accessing the FileInfo.Length property.
+    static long GetFileLength(string fileName)
+    {
+        long retVal;
+
+        try
+        {
+            var fi = new FileInfo(fileName);
+            retVal = fi.Length;
+        }
+        catch (FileNotFoundException)
+        {
+            // If a file is no longer present,
+            // just add zero bytes to the total.
+            retVal = 0;
+        }
+
+        return retVal;
+    }
+}
