d61941be021471696625c562c572dd556b163ea9
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-08-24 19:26:03 +0800

    create interfaces

---
+++ DC.Lab/ILight.cs
@@ -0,0 +1,20 @@
+﻿namespace DC.Lab;
+
+public interface ILight
+{
+    void SwitchOn();
+    void SwitchOff();
+    bool IsOn();
+}
+
+public interface ITimerLight : ILight
+{
+    public async Task TurnOnFor(int duration)
+    {
+        Console.WriteLine("Using the default interface method for the ITimerLight.TurnOnFor.");
+        SwitchOn();
+        await Task.Delay(duration);
+        SwitchOff();
+        Console.WriteLine("Completed ITimerLight.TurnOnFor sequence.");
+    }
+}
---
+++ DC.Lab/Light.cs
@@ -0,0 +1,45 @@
+﻿namespace DC.Lab;
+
+public class OverheadLight : ITimerLight
+{
+    private bool isOn;
+
+    public bool IsOn() => isOn;
+
+    public void SwitchOff() => isOn = false;
+
+    public void SwitchOn() => isOn = true;
+
+    public override string ToString()
+        => $"The light is {(isOn ? "on" : "off")}";
+}
+
+public class HalogenLight : ITimerLight
+{
+    private enum HalogenLightState
+    {
+        Off,
+        On,
+        TimerModeOn
+    }
+
+    private HalogenLightState state;
+
+    public void SwitchOn() => state = HalogenLightState.On;
+
+    public void SwitchOff() => state = HalogenLightState.Off;
+
+    public bool IsOn() => state != HalogenLightState.Off;
+
+    public async Task TurnOnFor(int duration)
+    {
+        Console.WriteLine("Halogen light starting timer function.");
+        state = HalogenLightState.TimerModeOn;
+        await Task.Delay(duration);
+        state = HalogenLightState.Off;
+        Console.WriteLine("Halogen light finished custom timer function.");
+    }
+
+    public override string ToString()
+        => $"The light is {state}";
+}
