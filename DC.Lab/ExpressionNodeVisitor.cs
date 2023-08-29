0141f507c3c949e8b2e3fa8112c2a62b6332f4db
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-08-29 20:21:50 +0800

    implement demo expression visitor

---
+++ DC.Lab/ExpressionNodeVisitor.cs
@@ -0,0 +1,167 @@
+﻿using System.Linq.Expressions;
+
+namespace DC.Lab;
+
+public abstract class Visitor
+{
+    private readonly Expression node;
+
+    protected Visitor(Expression node) => this.node = node;
+
+    public abstract void Visit(string prefix);
+
+    public ExpressionType NodeType => node.NodeType;
+
+    public static Visitor CreateFromExpression(Expression node) =>
+        node.NodeType switch
+        {
+            ExpressionType.Constant => new ConstantVisitor((ConstantExpression)node),
+            ExpressionType.Lambda => new LambdaVisitor((LambdaExpression)node),
+            ExpressionType.Parameter => new ParameterVisitor((ParameterExpression)node),
+            ExpressionType.Add => new BinaryVisitor((BinaryExpression)node),
+            ExpressionType.Equal => new BinaryVisitor((BinaryExpression)node),
+            ExpressionType.Multiply => new BinaryVisitor((BinaryExpression)node),
+            ExpressionType.Conditional => new ConditionalVisitor((ConditionalExpression)node),
+            ExpressionType.Call => new MethodCallVisitor((MethodCallExpression)node),
+            _ => throw new NotImplementedException($"Node not processed yet: {node.NodeType}")
+        };
+}
+
+// Lambda visitor
+public class LambdaVisitor : Visitor
+{
+    private readonly LambdaExpression node;
+
+    public LambdaVisitor(LambdaExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This expression is a {NodeType} expression type.");
+        Console.WriteLine($"{prefix}The name of the lambda is {((node.Name == null) ? "<null>" : node.Name)}");
+        Console.WriteLine($"{prefix}The return type is {node.ReturnType}");
+        Console.WriteLine($"{prefix}The expression has {node.Parameters.Count} argument(s). They are:");
+
+        // Visit each parameter
+        foreach (var argumentExpression in node.Parameters)
+        {
+            var argumentVisitor = CreateFromExpression(argumentExpression);
+            argumentVisitor.Visit(prefix + "\t");
+        }
+
+        Console.WriteLine($"{prefix}The expression body is:");
+
+        // Visit the body
+        var bodyVisitor = CreateFromExpression(node.Body);
+        bodyVisitor.Visit(prefix + "\t");
+    }
+}
+
+// Binary visitor
+public class BinaryVisitor : Visitor
+{
+    private readonly BinaryExpression node;
+
+    public BinaryVisitor(BinaryExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This binary expression is a {NodeType} expression");
+
+        var left = CreateFromExpression(node.Left);
+        Console.WriteLine($"{prefix}The left argument is:");
+        left.Visit(prefix + "\t");
+
+        var right = CreateFromExpression(node.Right);
+        Console.WriteLine($"{prefix} The right argument is:");
+        right.Visit(prefix + "\t");
+    }
+}
+
+// Paramerer visitor
+public class ParameterVisitor : Visitor
+{
+    private readonly ParameterExpression node;
+
+    public ParameterVisitor(ParameterExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This is an {NodeType} expression type.");
+        Console.WriteLine($"{prefix}Type: {node.Type}, Name: {node.Name}, ByRef: {node.IsByRef}");
+    }
+}
+
+// Constant visitor
+public class ConstantVisitor : Visitor
+{
+    private readonly ConstantExpression node;
+
+    public ConstantVisitor(ConstantExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This is an {NodeType} expression type.");
+        Console.WriteLine($"{prefix}The type of the constant value is {node.Type}");
+        Console.WriteLine($"{prefix}The value of the constant value is {node.Value}");
+    }
+}
+
+// Conditional visitor
+public class ConditionalVisitor : Visitor
+{
+    private readonly ConditionalExpression node;
+
+    public ConditionalVisitor(ConditionalExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This expression is a {NodeType} expression");
+
+        var testVisitor = CreateFromExpression(node.Test);
+        Console.WriteLine($"{prefix}The test for this expression is:");
+        testVisitor.Visit(prefix + "\t");
+
+        var trueVisitor = CreateFromExpression(node.IfTrue);
+        Console.WriteLine($"{prefix}The true clause for this expression is:");
+        testVisitor.Visit(prefix + "\t");
+
+        var falseVisitor = CreateFromExpression(node.IfFalse);
+        Console.WriteLine($"{prefix}The false clause for this expression is:");
+        testVisitor.Visit(prefix + "\t");
+    }
+}
+
+// Method call visitor
+public class MethodCallVisitor : Visitor
+{
+    private readonly MethodCallExpression node;
+
+    public MethodCallVisitor(MethodCallExpression node) : base(node) => this.node = node;
+
+    public override void Visit(string prefix)
+    {
+        Console.WriteLine($"{prefix}This expression is a {NodeType} expression");
+
+        if (node.Object is null)
+        {
+            Console.WriteLine($"{prefix}This is a static method call.");
+        }
+        else
+        {
+            Console.WriteLine($"{prefix}The receiver (this) is:");
+            var receiverVisitor = CreateFromExpression(node.Object);
+            receiverVisitor.Visit(prefix + "\t");
+        }
+
+        var methodInfo = node.Method;
+        Console.WriteLine($"{prefix}The method name is {methodInfo.DeclaringType}.{methodInfo.Name}");
+
+        // There is more here, like generic arguments, and so on.
+        Console.WriteLine($"{prefix}The arguments are:");
+        foreach (var arg in node.Arguments)
+        {
+            var argVisitor = CreateFromExpression(arg);
+            argVisitor.Visit(prefix + "\t");
+        }
+    }
+}
--- DC.Lab/InterpretingExpressionTrees.cs
+++ DC.Lab/InterpretingExpressionTrees.cs
@@ -6,6 +6,15 @@ public class InterpretingExpressionTrees
 {
     public static void Execute()
     {
+        //DecomposeLambda();
+
+        //DemoAdditionExpression();
+        //UsingTheVisitorFactory();
+        DeconstructingAComplexExpression();
+    }
+
+    static void DecomposeLambda()
+    {
         Expression<Func<int, bool>> exprTree = num => num < 5;
 
         ParameterExpression param = (ParameterExpression)exprTree.Parameters[0];
@@ -14,5 +23,52 @@ public class InterpretingExpressionTrees
         ConstantExpression right = (ConstantExpression)operation.Right;
 
         Console.WriteLine($"Decomposed expression: {param.Name} => {left.Name} {operation.NodeType} {right.Value}");
+    }
+
+    static void DemoAdditionExpression()
+    {
+        Expression<Func<int, int, int>> addition = (a, b) => a + b;
+
+        Console.WriteLine($"This expression is a {addition.NodeType} expression type");
+        Console.WriteLine($"The name of the lambda is {((addition.Name == null) ? "<null>" : addition.Name)}");
+        Console.WriteLine($"The return type is {addition.ReturnType.ToString()}");
+        Console.WriteLine($"The expression has {addition.Parameters.Count} arguments. They are:");
+
+        foreach (var argumentExpression in addition.Parameters)
+            Console.WriteLine($"\tParameter Type: {argumentExpression.Type.ToString()}, Name: {argumentExpression.Name}");
+
+        var additionBody = (BinaryExpression)addition.Body;
+
+        Console.WriteLine($"The body is a {additionBody.NodeType} expression");
+        Console.WriteLine($"The left side is a {additionBody.Left.NodeType} expression");
+
+        var left = (ParameterExpression)additionBody.Left;
+
+        Console.WriteLine($"\tParameter Type: {left.Type.ToString()}, Name: {left.Name}");
+        Console.WriteLine($"The right side is a {additionBody.Right.NodeType} expression");
+
+        var right = (ParameterExpression)additionBody.Right;
+
+        Console.WriteLine($"\tParameter Type: {right.Type.ToString()}, Name: {right.Name}");
+    }
+
+    static void UsingTheVisitorFactory()
+    {
+        Expression<Func<int, int, int>> addition = (a, b) => a + b;
+        var visitor = new LambdaVisitor(addition);
+        visitor.Visit("");
+    }
+
+    static void DeconstructingAComplexExpression()
+    {
+        Expression<Func<int, int>> factorial = (n) =>
+            n == 0 ?
+                1 :
+                Enumerable.Range(1, n)
+                    .Aggregate((product, factor) =>
+                        product + factor);
+
+        var visitor = new LambdaVisitor(factorial);
+        visitor.Visit("");
     }
 }
