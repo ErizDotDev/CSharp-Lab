af616d445aaebdb9a263fcbf0ecd02bed2142d56
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-10-14 19:24:39 +0800

    implement lambda vs. local function example

---
+++ DC.Lab/LambdaVersusLocalFunction.cs
@@ -0,0 +1,24 @@
+﻿namespace DC.Lab;
+
+public class LambdaVersusLocalFunction
+{
+    public static int LocalFunctionFactorial(int n)
+    {
+        return nthFactorial(n);
+
+        int nthFactorial(int number) => number < 2
+            ? 1
+            : number * nthFactorial(number - 1);
+    }
+
+    public static int LambdaFactorial(int n)
+    {
+        Func<int, int> nthFactorial = default(Func<int, int>)!;
+
+        nthFactorial = number => number < 2
+            ? 1
+            : number * nthFactorial(number - 1);
+
+        return nthFactorial(n);
+    }
+}
