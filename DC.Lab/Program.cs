d8ceede643ca430db6d324b3c55bdaf570d29fe5
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-10-24 22:04:22 +0800

    implement simple demo

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,64 @@
-﻿
+﻿namespace DC.Lab;
+
+public class Bus
+{
+    // Static variable used by all Bus instances.
+    // Represents the time the first bus of the day starts its route.
+    protected static readonly DateTime globalStartTime;
+
+    // Property for the number of each bus.
+    protected int RouteNumber { get; set; }
+
+    // Static constructor to initialize the static variable.
+    // It is invoked before the first instance constructor is run.
+    static Bus()
+    {
+        globalStartTime = DateTime.Now;
+
+        // The following statement produces the first line of output,
+        // and the line occurs only once.
+        Console.WriteLine($"Static constructor sets global start time to {globalStartTime.ToLongTimeString()}");
+    }
+
+    // Instance constructor
+    public Bus(int routeNum)
+    {
+        RouteNumber = routeNum;
+        Console.WriteLine($"Bus #{RouteNumber} is created.");
+    }
+
+    // Instance method.
+    public void Drive()
+    {
+        TimeSpan elapsedTime = DateTime.Now - globalStartTime;
+
+        // For demonstration purposes, we treat milliseconds as minutes to simulate
+        // actual bus times. Do not do this in your actual bus schedule program!
+        Console.WriteLine($"{this.RouteNumber} is starting its route {elapsedTime.Milliseconds:N2} minutes after global start time {globalStartTime.ToShortTimeString()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // The creation of this instance activates the static constructor.
+        Bus bus1 = new Bus(71);
+
+        // Create a second bus.
+        Bus bus2 = new Bus(72);
+
+        // Send bus1 on its way.
+        bus1.Drive();
+
+        // Wait for bus2 to warm up.
+        Thread.Sleep(25);
+
+        // Send bus2 on its way.
+        bus2.Drive();
+
+        // Keep the console window open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
