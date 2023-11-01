fe8b77b8b68729e94b14ab7c7d39d1604be6677b
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2023-11-01 21:39:42 +0800

    implement demo code

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using Day = System.DayOfWeek;
+
+namespace DC.Lab;
+
+class DayCollection
+{
+    string[] days = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+    // Indexer with only a get accessor with the expression-bodied definition:
+    public int this[string day] => FindDayIndex(day);
+
+    private int FindDayIndex(string day)
+    {
+        for (int j = 0; j < days.Length; j++)
+        {
+            if (days[j] == day)
+                return j;
+        }
+
+        throw new ArgumentOutOfRangeException(
+            nameof(day),
+            $"Day {day} is not supported.\nDay input must be in the form \"Sun\", \"Mon\", etc."
+        );
+    }
+}
+
+class DayOfWeekCollection
+{
+    Day[] days = {
+        Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday, Day.Saturday, Day.Sunday
+    };
+
+    // Indexer with only a get accessor with the expression-bodied definition:
+    public int this[Day day] => FindDayIndex(day);
+
+    private int FindDayIndex(Day day)
+    {
+        for (int j = 0; j < days.Length; j++)
+        {
+            if (days[j] == day)
+                return j;
+        }
+
+        throw new ArgumentOutOfRangeException(
+            nameof(day),
+            $"Day {day} is not supported.\nDay input must be in the form \"Sun\", \"Mon\", etc."
+        );
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var week = new DayOfWeekCollection();
+        Console.WriteLine(week[DayOfWeek.Friday]);
+
+        try
+        {
+            Console.WriteLine(week[(DayOfWeek)43]);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine($"Not supported input: {e.Message}");
+        }
+    }
+}
