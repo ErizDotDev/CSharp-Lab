577a97afb0ed35544d155c0c3c0f9432558fbfc4
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-04 22:01:43 +0800

    add code demonstrating class generic constraint and equality ops

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Text;
+
+class Program
+{
+    public static void OpEqualsTest<T>(T s, T t) where T : class
+    {
+        Console.WriteLine(s == t);
+    }
+
+    public static void Main(string[] args)
+    {
+        string s1 = "target";
+        StringBuilder sb = new("target");
+        string s2 = sb.ToString();
+        OpEqualsTest<string>(s1, s2);
+    }
+}
