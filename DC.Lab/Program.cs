8761a80fcd6ebab7fb6918bbd8c2d566a25587ed
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-05 21:35:53 +0800

    add simple demo code

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿namespace DC.Lab;
+
+class Program
+{
+    interface IFruit
+    {
+        static abstract bool HasSeeds { get; }
+    }
+
+    record Watermelon : IFruit
+    {
+        public static bool HasSeeds => true;
+    }
+
+    record Grape : IFruit
+    {
+        public static bool HasSeeds => false;
+    }
+
+    static void DisplayHasSeeds<T>(T fruit) where T : IFruit
+    {
+        Console.WriteLine(T.HasSeeds);
+    }
+
+    static void Main()
+    {
+        DisplayHasSeeds(new Watermelon());
+        DisplayHasSeeds(new Grape());
+    }
+}
