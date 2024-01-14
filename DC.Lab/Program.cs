674c567c9ecd3058cddd22e2c47a741327d22e8c
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-14 19:40:18 +0800

    add demo code

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿namespace DC.Lab;
+
+delegate void StackEventHandler<T, U>(T sender, U eventArgs);
+
+class Stack<T>
+{
+    public class StackEventArgs : EventArgs { }
+    public event StackEventHandler<Stack<T>, StackEventArgs>? StackEvent;
+
+    protected virtual void OnStackChanged(StackEventArgs a)
+    {
+        if (StackEvent is not null)
+            StackEvent(this, a);
+    }
+}
+
+class SampleClass
+{
+    public void HandleStackChange<T>(Stack<T> stack, Stack<T>.StackEventArgs args) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Stack<double> s = new Stack<double>();
+        SampleClass o = new SampleClass();
+        s.StackEvent += o.HandleStackChange;
+    }
+}
