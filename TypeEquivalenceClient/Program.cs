ea4f4f34ce3d70cb62ddee19d6f2be81fd905bff
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-28 21:02:42 +0800

    create client class

---
+++ TypeEquivalenceClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using TypeEquivalenceInterface;
+
+namespace TypeEquivalenceClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly sampleAssembly = Assembly.Load("TypeEquivalenceRuntime");
+
+            ISampleInterface sampleClass =
+                (ISampleInterface)sampleAssembly.CreateInstance("TypeEquivalenceRuntime.SampleClass");
+            sampleClass.GetUserInput();
+
+            Console.WriteLine(sampleClass.UserInput);
+            Console.WriteLine(sampleAssembly.GetName().Version.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
