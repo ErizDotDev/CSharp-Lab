9fdb30b8321ba9dce4ada69fae6e1b558eebebc7
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-28 20:51:04 +0800

    create interface

---
+++ TypeEquivalenceInterface/ISampleInterface.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TypeEquivalenceInterface
+{
+    [ComImport]
+    [Guid("5E3EFC5F-8347-43AA-984A-C89A5A0BA1AC")]
+    public interface ISampleInterface
+    {
+        void GetUserInput();
+        string UserInput { get; }
+    }
+}
