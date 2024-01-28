110476fd2adbc3d453b51541be8cdf4197895427
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-01-28 20:57:12 +0800

    create runtime class

---
+++ TypeEquivalenceRuntime/SampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TypeEquivalenceInterface;
+
+namespace TypeEquivalenceRuntime
+{
+    public class SampleClass : ISampleInterface
+    {
+        private string _userInput;
+        public string UserInput { get { return _userInput; } }
+
+        public void GetUserInput()
+        {
+            Console.WriteLine("Please enter a value:");
+            _userInput = Console.ReadLine();
+        }
+    }
+}
