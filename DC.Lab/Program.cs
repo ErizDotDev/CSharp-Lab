1d1e17f2ac6c4ec4c70f43536c10a1955b0e481c
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-02-13 20:49:17 +0800

    add demo code

--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace DC.Lab;
+
+class Program
+{
+    public static void Main()
+    {
+        SByte sbyteValue = -120;
+        ByteWithSign value = sbyteValue;
+        Console.WriteLine(value);
+
+        value = Byte.MaxValue;
+        Console.WriteLine(value);
+    }
+}
