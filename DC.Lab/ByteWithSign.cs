6b723e1217a7d583dafdaa111df9312e4ec5ca7a
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-02-13 21:15:04 +0800

    add struct

---
+++ DC.Lab/ByteWithSign.cs
@@ -0,0 +1,28 @@
+﻿namespace DC.Lab;
+
+public struct ByteWithSign
+{
+    private SByte signValue;
+    private Byte value;
+
+    public static implicit operator ByteWithSign(SByte value)
+    {
+        ByteWithSign newValue;
+        newValue.signValue = (SByte)Math.Sign(value);
+        newValue.value = (byte)Math.Abs(value);
+        return newValue;
+    }
+
+    public static implicit operator ByteWithSign(Byte value)
+    {
+        ByteWithSign newValue;
+        newValue.signValue = 1;
+        newValue.value = value;
+        return newValue;
+    }
+
+    public override string ToString()
+    {
+        return (signValue * value).ToString();
+    }
+}
