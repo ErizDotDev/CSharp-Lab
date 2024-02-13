41f0e252f6d32eb5b8d905fa9f31a08fcc0469e1
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-02-13 21:17:26 +0800

    add demo code

---
+++ DC.Lab/ByteWithSignE.cs
@@ -0,0 +1,40 @@
+﻿namespace DC.Lab;
+
+public struct ByteWithSignE
+{
+    private SByte signValue;
+    private Byte value;
+
+    private const byte MaxValue = byte.MaxValue;
+    private const int MinValue = -1 * byte.MaxValue;
+
+    public static explicit operator ByteWithSignE(int value)
+    {
+        // Check for overflow
+        if (value > ByteWithSignE.MaxValue || value < ByteWithSignE.MinValue)
+            throw new OverflowException($"{value} is out of range of the ByteWithSignE" +
+                $"data type");
+
+        ByteWithSignE newValue;
+        newValue.signValue = (SByte)Math.Sign(value);
+        newValue.value = (byte)Math.Abs(value);
+        return newValue;
+    }
+
+    public static explicit operator ByteWithSignE(uint value)
+    {
+        if (value > ByteWithSignE.MaxValue)
+            throw new OverflowException($"{value} is out of range of the ByteWithSignE" +
+                $"data type");
+
+        ByteWithSignE newValue;
+        newValue.signValue = 1;
+        newValue.value = (byte)value;
+        return newValue;
+    }
+
+    public override string ToString()
+    {
+        return (signValue * value).ToString();
+    }
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿namespace DC.Lab;
+
+class Program
+{
+    public static void Main()
+    {
+        ByteWithSignE value;
+
+        try
+        {
+            int intValue = -120;
+            value = (ByteWithSignE)intValue;
+            Console.WriteLine(value);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            uint uintValue = 1024;
+            value = (ByteWithSignE)uintValue;
+            Console.WriteLine(value);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
