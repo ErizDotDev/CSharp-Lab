a55c0cc855d9f7bc2a276d4fe7e6f85a8f96bbb0
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-02-25 17:09:43 +0800

    add gethashcode for numeric values

---
+++ DC.Lab/Number.cs
@@ -0,0 +1,25 @@
+﻿namespace DC.Lab;
+
+public struct Number
+{
+    private int n;
+
+    public Number(int value)
+    {
+        n = value;
+    }
+
+    public int Value => n;
+
+    public override bool Equals(Object obj)
+    {
+        if (obj is null || !(obj is Number))
+            return false;
+        else
+            return n == ((Number)obj).n;
+    }
+
+    public override int GetHashCode() => n;
+
+    public override string ToString() => n.ToString();
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿namespace DC.Lab;
+
+class Program
+{
+    static void Main()
+    {
+        // EXAMPLE 1
+        Console.WriteLine("Printing example 1...\n");
+        var rnd = new Random();
+
+        for (int ctr = 0; ctr <= 9; ctr++)
+        {
+            int randomN = rnd.Next(Int32.MinValue, Int32.MaxValue);
+            var n = new Number(randomN);
+            Console.WriteLine($"n = {n,12}, hash code = {n.GetHashCode(),12}");
+        }
+    }
+}
