ace139a5714aa08de18fe9ddb001c4f52473f5ad
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-02-25 17:22:07 +0800

    add gethashcode with struct in multiple props

---
+++ DC.Lab/Point2.cs
@@ -0,0 +1,24 @@
+﻿namespace DC.Lab;
+
+public struct Point2
+{
+    private int x;
+    private int y;
+
+    public Point2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override bool Equals(Object obj)
+    {
+        if (!(obj is Point2))
+            return false;
+
+        var p = (Point2)obj;
+        return x == p.x && y == p.y;
+    }
+
+    public override int GetHashCode() => x ^ y;
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         // EXAMPLE 1
         Console.WriteLine("Printing example 1...\n");
+
         var rnd = new Random();
 
         for (int ctr = 0; ctr <= 9; ctr++)
@@ -14,5 +15,14 @@ class Program
             var n = new Number(randomN);
             Console.WriteLine($"n = {n,12}, hash code = {n.GetHashCode(),12}");
         }
+
+        // EXAMPLE 2
+        Console.WriteLine("\nPrinting example 2...\n");
+
+        var pt = new Point2(5, 8);
+        Console.WriteLine(pt.GetHashCode());
+
+        pt = new Point2(8, 5);
+        Console.WriteLine(pt.GetHashCode());
     }
 }
