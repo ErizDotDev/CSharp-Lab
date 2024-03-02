65f7c8d3569858d6062621b12c5e060ca9b8dc24
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-03-02 14:46:22 +0800

    implement first code sample

---
+++ DC.Lab/Automobile.cs
@@ -0,0 +1,37 @@
+﻿namespace DC.Lab;
+
+public class Automobile
+{
+    public int Doors { get; init; }
+    public string Cylinders { get; init; } = null!;
+    public int Year { get; init; }
+    public string Model { get; init; }
+
+    public Automobile(string model, int year, int doors, string cylinders)
+    {
+        Model = model;
+        Year = year;
+        Doors = doors;
+        Cylinders = cylinders;
+    }
+
+    public override string ToString() => ToString("G");
+
+    public string ToString(string format)
+    {
+        if (string.IsNullOrEmpty(format))
+            format = "G";
+
+        var result = format.ToUpperInvariant() switch
+        {
+            "G" => $"{Year} {Model}",
+            "D" => $"{Year} {Model}, {Doors} dr.",
+            "C" => $"{Year} {Model}, {Cylinders}",
+            "A" => $"{Year} {Model}, {Doors} dr. {Cylinders}",
+            _ => throw new ArgumentException(
+                $"'{format}' is an invalid format string.")
+        };
+
+        return result;
+    }
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace DC.Lab;
+
+public class Program
+{
+    static void Main()
+    {
+        ExecuteFirstCodeSample();
+    }
+
+    static void ExecuteFirstCodeSample()
+    {
+        var auto = new Automobile("Lynx", 2016, 4, "V8");
+
+        Console.WriteLine(auto.ToString());
+        Console.WriteLine(auto.ToString("A"));
+    }
+}
