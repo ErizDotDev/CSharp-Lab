8dc70b2eb29b36c619fd33c631698e9188a6d67b
Denzel Eriz dela Cruz <denzelerizdc.dev@hotmail.com>
2024-03-19 23:44:52 +0800

    implement first example

---
+++ DC.Lab/Counter1.cs
@@ -0,0 +1,22 @@
+﻿namespace DC.Lab;
+
+internal class Counter1
+{
+    private int threshold;
+    private int total;
+
+    public Counter1(int passedThreshold)
+    {
+        threshold = passedThreshold;
+        ThresholdReached = default!;
+    }
+
+    public void Add(int x)
+    {
+        total += x;
+        if (total >= threshold)
+            ThresholdReached?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler ThresholdReached;
+}
--- DC.Lab/Program.cs
+++ DC.Lab/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿namespace DC.Lab;
+
+class Program
+{
+    static void Main()
+    {
+        var c = new Counter1(new Random().Next(10));
+
+        c.ThresholdReached += c_ThresholdReached!;
+
+        Console.WriteLine("Press 'a' key to increase total");
+
+        while (Console.ReadKey(true).KeyChar == 'a')
+        {
+            Console.WriteLine("adding one");
+            c.Add(1);
+        }
+    }
+
+    static void c_ThresholdReached(object sender, EventArgs e)
+    {
+        Console.WriteLine("The threshold was reached.");
+        Environment.Exit(0);
+    }
+}
