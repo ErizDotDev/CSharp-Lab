23f8414493ad44504650277e58d05ddc58a7b6c3
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-02-28 21:37:17 +0800

    demo definition of a custom interface

---
+++ DC.Lab.ProCSharp/Ch8/IPointy.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomInterfaces;
+
+//This interface defines the behavior of "having points".
+public interface IPointy
+{
+    //Implicitly public and abstract.
+    byte GetNumberOfPoints();
+
+    //A read-write property in an interface would look like:
+    //string PropName { get; set; }
+
+    //while a read-only property in an interface would be:
+    byte Points { get; }
+}
