12245d67dbc3368a78b3d94fb4a0f802253f2ca0
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-02-28 20:50:44 +0800

    implemented sample abstract class

---
+++ DC.Lab.ProCSharp/Ch8/CustomInterfaces.cs
@@ -0,0 +1,8 @@
+﻿namespace DC.Lab.ProCSharp.Ch8;
+
+public abstract class CustomInterfaces
+{
+    //Only derived types can support this "poymorphic interface".
+    //Classes in other hierarchies have no access to this abstract member.
+    public abstract object Clone();
+}
