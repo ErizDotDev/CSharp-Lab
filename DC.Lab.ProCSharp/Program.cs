d5adff8d4b9c94fb781a3b31b97675e2a3b9ccfa
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-02-28 20:57:41 +0800

    used interface as parameter

--- DC.Lab.ProCSharp/Program.cs
+++ DC.Lab.ProCSharp/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using CustomInterfaces;
+
+Console.WriteLine("***** A First Look at Interfaces *****\n");
+CloneableExample();
+
+static void CloneableExample()
+{
+    //All of these classes support the ICloneable interface.
+    string myStr = "Hello";
+    var unixOs = new OperatingSystem(PlatformID.Unix, new Version());
+
+    //Therefore, they can all be passed into a method taking ICloneable.
+    CloneMe(myStr);
+    CloneMe(unixOs);
+
+    static void CloneMe(ICloneable c)
+    {
+        //Clone whatever we get and print out the name.
+        object theClone = c.Clone();
+        Console.WriteLine($"Your clone is a: {theClone.GetType().Name}");
+    }
+}
+
+Console.ReadLine();
