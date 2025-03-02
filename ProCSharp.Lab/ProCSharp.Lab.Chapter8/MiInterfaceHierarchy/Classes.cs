65d0c218609f5013d310560eb7515a920125ca3d
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-03-02 21:49:22 +0800

    demo multiple inheritance with inherit types

---
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter8/MiInterfaceHierarchy/Classes.cs
@@ -0,0 +1,25 @@
+﻿namespace MiInterfaceHierarchy;
+
+class Rectangle : IShape
+{
+    public int GetNumberOfSides() => 4;
+
+    public void Draw() => Console.WriteLine("Drawing...");
+
+    public void Print() => Console.WriteLine("Printing...");
+}
+
+class Square : IShape
+{
+    //Using explicit implementation to handle member name clash.
+    void IPrintable.Draw() => Console.WriteLine("Draw to printer...");
+
+    void IAnotherDrawable.Draw() => Console.WriteLine("Draw to screen...");
+
+    public void Print()
+    {
+        //Print...
+    }
+
+    public int GetNumberOfSides() => 4;
+}
---
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter8/MiInterfaceHierarchy/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿namespace MiInterfaceHierarchy;
+
+//Multiple inheritance for interface types is A-okay.
+interface IAnotherDrawable
+{
+    void Draw();
+}
+
+interface IPrintable
+{
+    void Print();
+    void Draw(); // <-- Possible name clash here!
+}
+
+//Multiple interface inheritance. OK!
+interface IShape : IAnotherDrawable, IPrintable
+{
+    int GetNumberOfSides();
+}
