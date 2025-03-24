bab210990cdd96d9a91b6ee90b968b6981b75cd0
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-03-24 21:05:09 +0800

    demo simple garbage collection

---
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter9/Program.cs
@@ -0,0 +1,12 @@
+﻿using SimpleGC;
+
+Console.WriteLine("***** GC Basics *****");
+
+//Create a new Car object on the managed heap.
+//We are returned a reference to this object ("refToMyCar").
+Car refToMyCar = new Car("Zippy", 50);
+
+//The C# dot operator (.) is used to invoke members
+//on the object using our reference variable.
+Console.WriteLine(refToMyCar.ToString());
+Console.ReadLine();
---
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter9/SimpleGC/Classes.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleGC;
+
+public class Car
+{
+    public int CurrentSpeed { get; set; }
+    public string Name { get; set; }
+
+    public Car() { }
+    public Car(string name, int speed)
+    {
+        CurrentSpeed = speed;
+        Name = name;
+    }
+
+    public override string ToString() => $"{Name} is going {CurrentSpeed} MPH";
+}
