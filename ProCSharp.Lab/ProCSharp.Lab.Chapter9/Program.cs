d42af6c2a6aa8088da2748edfc6a1cffda199ada
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-04-03 22:44:18 +0800

    demo garbage collection generations

--- ProCSharp.Lab/ProCSharp.Lab.Chapter9/Program.cs
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter9/Program.cs
@@ -9,4 +9,19 @@ Car refToMyCar = new Car("Zippy", 50);
 //The C# dot operator (.) is used to invoke members
 //on the object using our reference variable.
 Console.WriteLine(refToMyCar.ToString());
+
+Console.WriteLine("\n***** GC Basics *****");
+
+//Print out estimated number of bytes on heap.
+Console.WriteLine($"Estimated bytes on heap: {GC.GetTotalMemory(false)}");
+
+//MaxGeneration is zero based, so add 1 for display purposes.
+Console.WriteLine($"This OS has {(GC.MaxGeneration + 1)} object generations");
+
+Car refToMyCar2 = new Car("Zippy", 50);
+Console.WriteLine(refToMyCar2.ToString());
+
+//Print out generation of refToMyCar object.
+Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar2)}");
+
 Console.ReadLine();
