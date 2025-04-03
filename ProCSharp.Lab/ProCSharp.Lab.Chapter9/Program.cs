2e3af07f3286cdc3e89e8bc72971f2d8e01785bc
Denzel dela Cruz <ddelacruz@gtreasury.com>
2025-04-03 22:53:38 +0800

    demo forced garbage collection

--- ProCSharp.Lab/ProCSharp.Lab.Chapter9/Program.cs
+++ ProCSharp.Lab/ProCSharp.Lab.Chapter9/Program.cs
@@ -10,7 +10,7 @@ Car refToMyCar = new Car("Zippy", 50);
 //on the object using our reference variable.
 Console.WriteLine(refToMyCar.ToString());
 
-Console.WriteLine("\n***** GC Basics *****");
+Console.WriteLine("\n***** Fun with System.GC *****");
 
 //Print out estimated number of bytes on heap.
 Console.WriteLine($"Estimated bytes on heap: {GC.GetTotalMemory(false)}");
@@ -23,5 +23,37 @@ Console.WriteLine(refToMyCar2.ToString());
 
 //Print out generation of refToMyCar object.
 Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar2)}");
+
+//Make a ton of objects for testing purposes.
+object[] tonsOfObjects = new object[50000];
+
+for (int i = 0; i < 50000; i++)
+{
+    tonsOfObjects[i] = new object();
+}
+
+//Collect only gen 0 objects.
+Console.WriteLine("Force Garbage Collection");
+GC.Collect(0, GCCollectionMode.Forced);
+GC.WaitForPendingFinalizers();
+
+//Print out generation of refToMyCar object.
+Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar2)}");
+
+//See if tonsOfObjects[9000] is still alive.
+if (tonsOfObjects[9000] != null)
+{
+    Console.WriteLine($"Generation of tonsOfObjects[9000] is: {GC.GetGeneration(tonsOfObjects[9000])}");
+}
+else
+{
+    Console.WriteLine("tonsOfObjects[9000] is no longer alive.");
+}
+
+//Print out how many times a generation has been swept.
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine($"Gen {i} has been swept {GC.CollectionCount(i)} times");
+}
 
 Console.ReadLine();
